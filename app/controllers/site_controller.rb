51384555912235b09f3819cc52ce9fe886abefb7
jacky-xbb <jacky-xbb@gmail.com>
2024-03-10 20:58:04 +0800

    first commit

---
+++ app/controllers/site_controller.rb
@@ -0,0 +1,16 @@
+class SiteController < ApplicationController
+  def first
+    @count = 3
+  end
+
+  def second; end
+
+  def third
+    @name, @email, @age = params[:person].values_at(:name, :email, :age)
+    @count = params[:count].to_i + 1
+    @post = Post.create(title: @name)
+    @post.broadcast_update_to('teststr', target: 'broadcasts')
+  end
+
+  def fourth; end
+end
